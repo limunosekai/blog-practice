c6c1f6924099e2bc9744fb878fb0cdb0af6fd027
limunosekai <powerlsh0103@gmail.com>
2022-04-05 21:43:56 +0900

    Add Post Func

--- client/src/pages/WritePage.js
+++ client/src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
-import TagBox from '../components/write/TagBox';
-import WriteActionButtons from '../components/write/WriteActionButtons';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 function WritePage() {
   return (
     <Responsive>
       <EditorContainer />
-      <TagBox />
-      <WriteActionButtons />
+      <TagBoxContainer />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 }
