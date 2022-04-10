893abc8ccd1df2daa23bd0068b568799f17ed2d4
limunosekai <powerlsh0103@gmail.com>
2022-04-10 22:06:02 +0900

    Add Modify Func & Modal Component

--- client/src/containers/post/PostViewerContainer.js
+++ client/src/containers/post/PostViewerContainer.js
@@ -1,17 +1,23 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
+import PostActionButton from '../../components/post/PostActionButton';
+import { setOriginalPost } from '../../modules/write';
 
 const PostViewerContainer = () => {
   const { postId } = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { post, error, loading } = useSelector(({ post, loading }) => ({
-    post: post.post,
-    error: post.error,
-    loading: loading['post/READ_POST'],
-  }));
+  const { post, error, loading, user } = useSelector(
+    ({ post, loading, user }) => ({
+      post: post.post,
+      error: post.error,
+      loading: loading['post/READ_POST'],
+      user: user.user,
+    }),
+  );
 
   useEffect(() => {
     dispatch(readPost(postId));
@@ -20,7 +26,21 @@ const PostViewerContainer = () => {
     };
   }, [dispatch, postId]);
 
-  return <PostViewer post={post} loading={loading} error={error} />;
+  const onEdit = () => {
+    dispatch(setOriginalPost(post));
+    navigate('/write');
+  };
+
+  const ownPost = (user && user._id) === (post && post.user._id);
+
+  return (
+    <PostViewer
+      post={post}
+      loading={loading}
+      error={error}
+      actionButtons={ownPost && <PostActionButton onEdit={onEdit} />}
+    />
+  );
 };
 
 export default PostViewerContainer;
